79aeb86c3871c59c3388026390ff2bafdfbf32d9
grokmann <grokmann@gmail.com>
2016-02-29 08:10:08 -0500

    Adding latest junk with clues

--- Kata02/grokmann/chop-01.rkt
+++ Kata02/grokmann/chop-01.rkt
@@ -3,7 +3,7 @@
 
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
-(define z (list 1 2 3 4))
+(define z (range 1 10))
 
 (define chop
   (lambda (x ls)
@@ -11,11 +11,13 @@
                   (if (pair? ls)
                       (cond
                         [(equal? (first ls) x)
-                         0]
+                         offset]
                         [else -1])
                       -1))])
       (f x ls 0))))
 
 (module+ test
   (chop 1 z)
+  (list-tail z (list-ref z (floor (/ (length z) 2))))
   )
+
