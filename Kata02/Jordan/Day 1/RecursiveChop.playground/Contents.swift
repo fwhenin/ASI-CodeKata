ab211c4e9b781f265c666196eeb84c7282e18c65
Jordan Camara <jcamara@autosoftdms.com>
2016-02-28 21:10:33 -0500

    Kata 02 - Day 1 - recursive binary search

---
+++ Kata02/Jordan/Day 1/RecursiveChop.playground/Contents.swift
@@ -0,0 +1,47 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func karateChop(target: Int, list: [Int]) -> Int {
+    let range = Range<Int>(start: list.startIndex, end: list.endIndex)
+    return recursiveChop(target, list: list, range: range)
+}
+
+func recursiveChop(target: Int, list: [Int], var range: Range<Int>) -> Int {
+    if list[range].isEmpty {
+        return -1
+    } else {
+        let centerIndex = (range.endIndex + range.startIndex) / 2
+        
+        if target < list[centerIndex] {
+            range.endIndex = centerIndex
+            return recursiveChop(target, list: list, range: range)
+        } else if target > list[centerIndex] {
+            range.startIndex = centerIndex + 1
+            return recursiveChop(target, list: list, range: range)
+        } else {
+            return centerIndex
+        }
+    }
+}
+
+assert(karateChop(3, list: []) == -1)
+assert(karateChop(1, list: [1]) == 0)
+assert(karateChop(3, list: [1, 2]) == -1)
+
+assert(karateChop(1, list: [1, 3, 5]) == 0)
+assert(karateChop(3, list: [1, 3, 5]) == 1)
+assert(karateChop(5, list: [1, 3, 5]) == 2)
+assert(karateChop(2, list: [1, 3, 5]) == -1)
+assert(karateChop(4, list: [1, 3, 5]) == -1)
+assert(karateChop(6, list: [1, 3, 5]) == -1)
+
+assert(karateChop(1, list: [1, 3, 5, 7]) == 0)
+assert(karateChop(3, list: [1, 3, 5, 7]) == 1)
+assert(karateChop(5, list: [1, 3, 5, 7]) == 2)
+assert(karateChop(7, list: [1, 3, 5, 7]) == 3)
+assert(karateChop(0, list: [1, 3, 5, 7]) == -1)
+assert(karateChop(2, list: [1, 3, 5, 7]) == -1)
+assert(karateChop(4, list: [1, 3, 5, 7]) == -1)
+assert(karateChop(6, list: [1, 3, 5, 7]) == -1)
+assert(karateChop(8, list: [1, 3, 5, 7]) == -1)
