767be8393d3583aa629ba07e601712bc0cfb15c7
grokmann <grokmann@gmail.com>
2016-03-16 22:27:17 -0400

    Chop2. I really dislike this code. It feels very needy.

--- Kata02/grokmann/c#/chop2.cs
+++ Kata02/grokmann/c#/chop2.cs
@@ -8,9 +8,41 @@ namespace CodeKata_02
 {
     public class Chop2
     {
-        public static int Chop(int x, int[] array)
+        public static int Chop(int target, int[] array)
         {
-            return -1;
+            var index = -1;
+            var start = 0;
+            var end = array.Length - 1;
+            var searchIndex = array.Length / 2;
+
+            while (array.Length > 0 && index != searchIndex)
+            {
+                searchIndex = start + ((end - start) / 2);
+
+                if (array[searchIndex] == target)
+                {
+                    index = searchIndex;
+                }
+                else if (searchIndex == start)
+                {
+                    if (searchIndex + 1 == end && array[end] == target)
+                    {
+                        index = end;
+                    }
+
+                    break;
+                }
+                else if (array[searchIndex] > target)
+                {
+                    end = searchIndex;
+                }
+                else
+                {
+                    start = searchIndex;
+                };
+            }
+
+            return index;
         }
     }
 }
