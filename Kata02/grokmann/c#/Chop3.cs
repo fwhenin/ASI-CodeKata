2aeb83fe725d610bd5ea7695a1a1a9bbcd6d80da
grokmann <grokmann@gmail.com>
2016-03-24 23:13:58 -0400

    Iteration 3. Recursive C# chop(). I think my code is getting uglier.

---
+++ Kata02/grokmann/c#/Chop3.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata_02
+{
+    public class Chop3
+    {
+        public static int Chop(int target, int[] array)
+        {
+            var index = -1;
+            var offset = 0;
+
+            if (array.Length > 0)
+            {
+                if (array.Length == 1)
+                {
+                    if (array[0] == target)
+                    {
+                        index = 0;
+                    }
+                }
+                else {
+                    offset = array.Length / 2;
+                    var newLength = 0;
+                    var newArray = new int[0];
+
+                    if (array[offset] > target)
+                    {
+                        newArray = new int[offset];
+                        Array.Copy(array, newArray, offset);
+                        offset = 0;
+                    }
+                    else
+                    {
+                        newLength = array.Length - offset;
+                        newArray = new int[newLength];
+                        Array.Copy(array, offset, newArray, 0, newLength);
+                    };
+
+                    var newResult = Chop(target, newArray);
+
+                    if (newResult == -1)
+                    {
+                        index = -1;
+                    }
+                    else {
+                        index = offset + newResult;
+                    }
+                }
+            }
+
+            return index;
+        }
+    }
+}
---
+++ Kata02/grokmann/c#/tests/TestChop.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Common;
+using NUnit.Framework;
+
+namespace CodeKata_02.tests
+{
+    [TestFixture]
+    public class TestChop
+    {
+        [TestCase(3, new int[] { }, ExpectedResult = -1, TestName = "Chop2 Test01")]
+        [TestCase(3, new int[] { 1 }, ExpectedResult = -1, TestName = "Chop2 Test02")]
+        [TestCase(1, new int[] { 1 }, ExpectedResult = 0, TestName = "Chop2 Test03")]
+        [TestCase(1, new int[] { 1, 3, 5 }, ExpectedResult = 0, TestName = "Chop2 Test04")]
+        [TestCase(3, new int[] { 1, 3, 5 }, ExpectedResult = 1, TestName = "Chop2 Test05")]
+        [TestCase(5, new int[] { 1, 3, 5 }, ExpectedResult = 2, TestName = "Chop2 Test06")]
+        [TestCase(0, new int[] { 1, 3, 5 }, ExpectedResult = -1, TestName = "Chop2 Test07")]
+        [TestCase(2, new int[] { 1, 3, 5 }, ExpectedResult = -1, TestName = "Chop2 Test08")]
+        [TestCase(4, new int[] { 1, 3, 5 }, ExpectedResult = -1, TestName = "Chop2 Test09")]
+        [TestCase(6, new int[] { 1, 3, 5 }, ExpectedResult = -1, TestName = "Chop2 Test10")]
+        [TestCase(1, new int[] { 1, 3, 5, 7 }, ExpectedResult = 0, TestName = "Chop2 Test11")]
+        [TestCase(3, new int[] { 1, 3, 5, 7 }, ExpectedResult = 1, TestName = "Chop2 Test12")]
+        [TestCase(5, new int[] { 1, 3, 5, 7 }, ExpectedResult = 2, TestName = "Chop2 Test13")]
+        [TestCase(7, new int[] { 1, 3, 5, 7 }, ExpectedResult = 3, TestName = "Chop2 Test14")]
+        [TestCase(0, new int[] { 1, 3, 5, 7 }, ExpectedResult = -1, TestName = "Chop2 Test15")]
+        [TestCase(2, new int[] { 1, 3, 5, 7 }, ExpectedResult = -1, TestName = "Chop2 Test16")]
+        [TestCase(4, new int[] { 1, 3, 5, 7 }, ExpectedResult = -1, TestName = "Chop2 Test17")]
+        [TestCase(6, new int[] { 1, 3, 5, 7 }, ExpectedResult = -1, TestName = "Chop2 Test18")]
+        [TestCase(8, new int[] { 1, 3, 5, 7 }, ExpectedResult = -1, TestName = "Chop2 Test19")]
+        public int Chop2Test(int index, int[] array)
+        {
+            return Chop2.Chop(index, array);
+        }
+
+        [TestCase(3, new int[] { }, ExpectedResult = -1, TestName = "Chop3 Test01")]
+        [TestCase(3, new int[] { 1 }, ExpectedResult = -1, TestName = "Chop3 Test02")]
+        [TestCase(1, new int[] { 1 }, ExpectedResult = 0, TestName = "Chop3 Test03")]
+        [TestCase(1, new int[] { 1, 3, 5 }, ExpectedResult = 0, TestName = "Chop3 Test04")]
+        [TestCase(3, new int[] { 1, 3, 5 }, ExpectedResult = 1, TestName = "Chop3 Test05")]
+        [TestCase(5, new int[] { 1, 3, 5 }, ExpectedResult = 2, TestName = "Chop3 Test06")]
+        [TestCase(0, new int[] { 1, 3, 5 }, ExpectedResult = -1, TestName = "Chop3 Test07")]
+        [TestCase(2, new int[] { 1, 3, 5 }, ExpectedResult = -1, TestName = "Chop3 Test08")]
+        [TestCase(4, new int[] { 1, 3, 5 }, ExpectedResult = -1, TestName = "Chop3 Test09")]
+        [TestCase(6, new int[] { 1, 3, 5 }, ExpectedResult = -1, TestName = "Chop3 Test10")]
+        [TestCase(1, new int[] { 1, 3, 5, 7 }, ExpectedResult = 0, TestName = "Chop3 Test11")]
+        [TestCase(3, new int[] { 1, 3, 5, 7 }, ExpectedResult = 1, TestName = "Chop3 Test12")]
+        [TestCase(5, new int[] { 1, 3, 5, 7 }, ExpectedResult = 2, TestName = "Chop3 Test13")]
+        [TestCase(7, new int[] { 1, 3, 5, 7 }, ExpectedResult = 3, TestName = "Chop3 Test14")]
+        [TestCase(0, new int[] { 1, 3, 5, 7 }, ExpectedResult = -1, TestName = "Chop3 Test15")]
+        [TestCase(2, new int[] { 1, 3, 5, 7 }, ExpectedResult = -1, TestName = "Chop3 Test16")]
+        [TestCase(4, new int[] { 1, 3, 5, 7 }, ExpectedResult = -1, TestName = "Chop3 Test17")]
+        [TestCase(6, new int[] { 1, 3, 5, 7 }, ExpectedResult = -1, TestName = "Chop3 Test18")]
+        [TestCase(8, new int[] { 1, 3, 5, 7 }, ExpectedResult = -1, TestName = "Chop3 Test19")]
+        public int Chop3Test(int index, int[] array)
+        {
+            return Chop3.Chop(index, array);
+        }
+    }
+}
