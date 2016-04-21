4f6f2c07a0861f458047e1a73076bacae84d57aa
Jordan Camara <jcamara@autosoftdms.com>
2016-04-20 21:34:22 -0400

    Optimizations and getting ready to share the love.

--- Kata04/Jordan/DataMunging/WeatherData/WeatherData/WeatherData.swift
+++ Kata04/Jordan/DataMunging/WeatherData/WeatherData/WeatherData.swift
@@ -11,30 +11,46 @@ import Foundation
 let kTrimCharacterSet = NSCharacterSet.decimalDigitCharacterSet().invertedSet
 
 class WeatherData {
-    var data: [[String]]!
+    var data: [DifferenceData]!
+    
+    internal struct DifferenceData {
+        var name: String
+        var difference: Double
+        
+        init(name: String, difference: Double) {
+            self.name = name
+            self.difference = difference
+        }
+    }
     
     init(data: String) {
         self.data = processData(data)
     }
     
     func getDayWithMinSpread() -> String {
-        var minDay = ""
-        var minTempRange = Double(Int.max)
-        for day in data[1..<data.endIndex] {
-            let max = Double(day[1].stringByTrimmingCharactersInSet(kTrimCharacterSet))!
-            let min = Double(day[2].stringByTrimmingCharactersInSet(kTrimCharacterSet))!
-            let currentRange = max - min
-            if currentRange < minTempRange {
-                minDay = day[0]
-                minTempRange = currentRange
-            }
-        }
-        
+        let minDay = data.minElement { $0.difference < $1.difference }!.name
         return minDay
     }
     
-    private func processData(data: String) -> [[String]] {
-        let result = data.characters.split("\n").map { $0.split(" ").map(String.init) }
+    private func processData(data: String) -> [DifferenceData] {
+        var result = [DifferenceData]()
+        
+        // split file by lines
+        let lines = data.characters.split("\n")
+        for line in lines[1..<lines.endIndex] {
+            
+            // split into columns
+            let columns = line.split(" ").map(String.init)
+            
+            // convert the minuend and subtrahend and calculate the difference up front
+            let minuend = Double(columns[1].stringByTrimmingCharactersInSet(kTrimCharacterSet))!
+            let subtrahend = Double(columns[2].stringByTrimmingCharactersInSet(kTrimCharacterSet))!
+            let differenceData = DifferenceData(name: columns[0], difference: abs(minuend - subtrahend))
+            
+            // append to result
+            result.append(differenceData)
+        }
+        
         return result
     }
 }
