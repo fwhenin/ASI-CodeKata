05b90293db7beafd8fb3f79fe0eb82690f65e4fe
grokmann <grokmann@gmail.com>
2016-06-04 11:48:31 -0400

    Actually working. Need to play with size of bit array to make it useful.

--- Kata05/grokmann/c#/BloomFilter/Program.cs
+++ Kata05/grokmann/c#/BloomFilter/Program.cs
@@ -1,27 +1,63 @@
 ﻿using System;
 using System.Text;
 using HashDepot;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections;
 
 namespace BloomFilter
 {
     class Program
     {
-        private const uint filterSize = 32;
-        private const uint seed = 9;
+        private const int filterSize = 1024;
+        private const string filename = @"..\..\data\wordlist.txt";
 
         static void Main(string[] args)
         {
-            var buffer = Encoding.ASCII.GetBytes("some string");
-            uint murmurHashResult = MurmurHash3.Hash32(buffer, seed);
-            var murmurValue = murmurHashResult % filterSize;
+            var bitArray = new BitArray(filterSize);
 
-            buffer = Encoding.ASCII.GetBytes("some string");
-            uint fnvHashResult = Fnv1a.Hash32(buffer); // 32-bit hash
-            var fnvValue = fnvHashResult % filterSize;
+            var wordlist = File.ReadAllLines(filename);
 
-            Console.WriteLine("Murmur Result:" + murmurValue);
-            Console.WriteLine("FNV Result:" + fnvValue);
+            foreach (var word in wordlist)
+            {
+                var murmurValue = (int)(GetMurmurHashValue(word) % filterSize);
+                bitArray.Set(murmurValue, true);
+
+                var fnvValue = (int)(GetFnvHashValue(word) % filterSize);
+                bitArray.Set(fnvValue, true);
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Enter a word to check:");
+                var testWord = Console.ReadLine();
+
+                if (bitArray.Get((int)(GetMurmurHashValue(testWord) % filterSize)) && bitArray.Get((int)(GetFnvHashValue(testWord) % filterSize)))
+                {
+                    Console.WriteLine("That word is _probably_ in the dictionary already.");
+                }
+                else
+                {
+                    Console.WriteLine("Huh. That's a new one.");
+                }
+            }
+
             Console.ReadKey();
+        }
+
+        private static uint GetMurmurHashValue(string word)
+        {
+            const uint seed = 9;
+            var encodedWord = Encoding.ASCII.GetBytes(word);
+            uint hashResult = MurmurHash3.Hash32(encodedWord, seed);
+            return hashResult;
+        }
+
+        private static uint GetFnvHashValue(string word)
+        {
+            var encodedWord = Encoding.ASCII.GetBytes(word);
+            uint hashResult = Fnv1a.Hash32(encodedWord);
+            return hashResult;
         }
     }
 }
